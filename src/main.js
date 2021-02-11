084b8602a44751a529bd76cea3a3fb099790c531
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2021-02-11 21:55:44 +0530

    Showing Articles on clicking on tags

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import App from './App.vue';
 
 import AllArticlesView from './views/AllArticlesView.vue';
 import TagsList from './views/TagsList.vue';
+import TagArticlesView from "@/views/TagArticlesView";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/articles', component: AllArticlesView},
-        { path: '/tags', component: TagsList}
+        { path: '/tags', component: TagsList},
+        { path: '/tags/:tag', component: TagArticlesView}
     ]
 });
 
---
+++ src/views/TagArticlesView.vue
@@ -0,0 +1,24 @@
+<template>
+  <ArticleList v-bind:api-url="baseURL"/>
+</template>
+
+<script>
+import {API_BASE_URL} from '/src/config.js';
+import ArticleList from '/src/components/lists/ArticleList.vue'
+
+export default {
+  name: "TagArticlesView",
+  data() {
+    return {
+      baseURL :  API_BASE_URL+'/tags/'+this.$route.params.tag,
+    }
+  },
+  components : {
+    ArticleList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
