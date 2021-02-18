dcd70279f066aa897a3685f6cff318e2015124ba
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2021-02-18 13:11:09 +0530

    Added Cover Image

--- src/components/cards/ArticleCard.vue
+++ src/components/cards/ArticleCard.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="card">
+  <!--Article Card-->
+  <div class="card mb-3">
+    <img class="card-img-top" v-bind:src="baseUrl+coverImage" alt="Card image cap">
     <div class="card-header">
       <span v-for="tag in tags" :key="tag">
         {{tag+' '}}
       </span>
-
     </div>
     <div class="card-body">
       <h5 class="card-title">{{title}}</h5>
@@ -12,17 +13,39 @@
       <a v-bind:href="'/articles/'+id" class="btn btn-primary">View Article</a>
     </div>
   </div>
+
+
+<!--  <div class="card">-->
+<!--    <div class="card-header">-->
+<!--      <span v-for="tag in tags" :key="tag">-->
+<!--        {{tag+' '}}-->
+<!--      </span>-->
+
+<!--    </div>-->
+<!--    <div class="card-body">-->
+<!--      <h5 class="card-title">{{title}}</h5>-->
+<!--      <p class="card-text">{{description}}</p>-->
+<!--      <a v-bind:href="'/articles/'+id" class="btn btn-primary">View Article</a>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script>
+import {API_BASE_URL} from '/src/config.js';
 export default {
   name: 'ArticleCard',
   props: [
     'title',
     'description',
     'tags',
-    'id'
-  ]
+    'id',
+    'coverImage'
+  ],
+  data() {
+    return {
+      baseUrl : API_BASE_URL
+    }
+  }
 }
 </script>
 
