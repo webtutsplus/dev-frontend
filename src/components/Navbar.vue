6fc47d38aa1a60ae1e3d3fda448115585a889c9d
Shivam Agarwal <shivam.agrawal.2000@gmail.com>
2021-03-01 23:07:23 +0530

    Removed unused Tag

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/"><img id="logo" src="/assets/icon.png" class="img-fluid" width="70%"/></a>
+    <a class="navbar-brand" href="/"><img id="logo" src="/assets/icon.png" class="img-fluid" /></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
