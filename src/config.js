15e5c9a7aa8fb0ead05c05c2a4535975931fbaeb
nmadhab <nilmadhab1994@booking.com>
2021-03-20 06:38:18 +0530

    port chnaged

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://api.simplecoding.dev/";
-export const ELASTIC_API_BASE_URL = "https://api.simplecoding.dev/search/feed_content"
+export const API_BASE_URL = "https://api.simplecoding.dev:3000/";
+export const ELASTIC_API_BASE_URL = "https://api.simplecoding.dev:3000/search/feed_content"
