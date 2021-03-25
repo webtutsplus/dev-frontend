e1bcb8d62d6495433e4bdf8b29a2297c01b52481
Smit Tailor <sjtailor27@gmail.com>
2021-03-25 12:59:22 +0530

    hamburger menu fixed for mobile devices.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import {createRouter, createWebHistory} from 'vue-router';
-
-
 import App from './App.vue';
-
+import "bootstrap";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import AllArticlesView from './views/AllArticlesView.vue';
 import TagsList from './views/TagsList.vue';
 import TagArticlesView from "@/views/TagArticlesView";
@@ -26,23 +23,23 @@ import AlgorithmsElastic from "@/views/Elastic/AlgorithmsElastic";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/articles', component: AllArticlesView},
-        { path: '/tags', component: TagsList},
-        { path: '/tags/:tag', component: TagArticlesView},
-        { path: '/articles/:slug', component: Article},
-        { path: '/', component: HomePageView},
-        { path: '/signup', component: SignUp},
-        { path: '/chrome-extension', component: ChromeExtensionPage},
-        { path: '/opensource' , component: OpenSourcePage},
-        { path: '/explore-courses', component: ExploreCoursesPage},
-        { path: '/signin', component: SignIn},
-        { path: '/backend', component:BackendElastic},
-        { path: '/frontend', component:FrontendElastic},
-        { path: '/devops', component:DevopsElastic},
-        { path: '/algorithms', component:AlgorithmsElastic},
-        { path: '/articles/:writer/:slug', component: Article},
-        { path: '/android', component: AndroidHomePage},
-        { path: '/signin', component: SignIn}
+        {path: '/articles', component: AllArticlesView},
+        {path: '/tags', component: TagsList},
+        {path: '/tags/:tag', component: TagArticlesView},
+        {path: '/articles/:slug', component: Article},
+        {path: '/', component: HomePageView},
+        {path: '/signup', component: SignUp},
+        {path: '/chrome-extension', component: ChromeExtensionPage},
+        {path: '/opensource', component: OpenSourcePage},
+        {path: '/explore-courses', component: ExploreCoursesPage},
+        {path: '/signin', component: SignIn},
+        {path: '/backend', component: BackendElastic},
+        {path: '/frontend', component: FrontendElastic},
+        {path: '/devops', component: DevopsElastic},
+        {path: '/algorithms', component: AlgorithmsElastic},
+        {path: '/articles/:writer/:slug', component: Article},
+        {path: '/android', component: AndroidHomePage},
+        {path: '/signin', component: SignIn}
     ]
 });
 router.beforeEach((toRoute, fromRoute, next) => {
